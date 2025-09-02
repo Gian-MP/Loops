02a591843914f2e253d55dececc44d0109f89f76
Gian-MP <202480071@psu.palawan.edu.ph>
2025-09-02 23:22:10 +0800

    Create Loop4.cs

---
+++ Loop4.cs
@@ -0,0 +1,16 @@
+
+using System;
+
+public class Loop
+{
+    public static void Main(string[] args)
+    {
+
+        for (int n = 1; n < 11; n++) {
+            
+            int squared = n * n;
+            Console.WriteLine(squared);
+            
+        }
+    }
+}
